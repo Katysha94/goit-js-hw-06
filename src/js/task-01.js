308bce72f27093e9937375603bbf92f56a8451a0
Katysha94 <Vasilevakata76@gmail.com>
2023-08-10 10:48:40 +0300

    task-1

---
+++ src/js/task-01.js
@@ -0,0 +1,19 @@
+const itemEl = document.querySelectorAll('.item');
+console.log('Number of categories:', itemEl.length);
+
+const categoriesList = document.querySelector('#categories')
+// console.log(categoriesList)
+
+const itemList = categoriesList.querySelectorAll('.item')
+// console.log(itemList)
+
+itemList.forEach(item => {
+    const h2El = item.querySelector('h2').innerHTML;
+    console.log('Category:', h2El);
+    const itemslendth = item.querySelectorAll('li').length
+    console.log(itemslendth)
+
+})
+
+
+
